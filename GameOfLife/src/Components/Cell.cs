087f5a7dcf1b57e189a440431f6ee5be2fa5cba6
magwrap <jasiek.musiol@gmail.com>
2024-03-04 16:20:51 +0100

    fix

--- GameOfLife/src/Components/Cell.cs
+++ GameOfLife/src/Components/Cell.cs
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 namespace GameOfLife;
 
 class Cell
 {
 
 }
+=======
+namespace GameOfLife.Components;
+
+// I AM A CELL
+public class Cell
+{
+    
+}
+>>>>>>> 450eb10 (created Cell class)
