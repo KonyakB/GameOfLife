031a71a34115c708026ec9980decc859cfea1c38
jan musiol <jasiek.musiol@gmail.com>
2024-03-06 16:18:37 +0100

    resolved merge conflict

--- GameOfLife/src/Components/Cell.cs
+++ GameOfLife/src/Components/Cell.cs
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-namespace GameOfLife;
-
-class Cell
-{
-
-}
-=======
 namespace GameOfLife.Components;
 
 // I AM A CELL
 public class Cell
 {
-    
+
 }
->>>>>>> 450eb10 (created Cell class)
