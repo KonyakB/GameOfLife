6be7cbd2ea4796d752f2814a0864075b84fc1465
jan musiol <jasiek.musiol@gmail.com>
2024-03-07 19:12:41 +0100

    Bence dummy forgot to add a reference from Testing directory to the Program directory and added CellTests Class

---
+++ GameOfLife.Tests/CellTests.cs
@@ -0,0 +1,22 @@
+using GameOfLife.Components;
+
+namespace GameOfLife.Tests;
+
+public class CellTests
+{ 
+    
+    // [InitializeData]
+    // public CellTests
+    // {
+    //
+    // }
+
+
+
+    [Fact]
+    public void AddOneNeighborTest()
+    {
+        var stubCell = new Cell();
+        
+    }
+}
