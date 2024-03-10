878b8245871fb1a66f278efbffa9f85d647dd837
KonyakB <bencekanyok@gmail.com>
2024-03-10 19:59:42 +0100

    IGrid interface & Grid class

---
+++ GameOfLife/src/Components/Grid.cs
@@ -0,0 +1,80 @@
+using System.Text.Json;
+using GameOfLife.Interfaces;
+
+namespace GameOfLife.Components;
+
+public class Grid : IGrid
+{
+    private int rows { get; set; }
+    private int columns { get; set; }
+    private Cell[,] grid { get; set; }
+    
+    public Grid(bool[][] boollist, int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        Cell[,] _grid = new Cell[rows, columns];
+        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                switch (boollist[i][j])
+                {
+                    case false:
+                        _grid[i, j].IsAlive = false;
+                        break;
+                    case true:
+                        _grid[i, j].IsAlive = true;
+                        break;
+                } 
+            }
+        }
+        
+        Neighbors(_grid);
+        _grid = grid;
+    }
+
+    public void Neighbors(Cell[,] grid)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int iTop = (i > 0) ? i - 1 : rows - 1;
+                int iBottom = (i < rows - 1) ? i + 1 : 0;
+                
+                int jLeft = (j > 0) ? j - 1 : columns - 1;
+                int jRight = (j < columns - 1) ? j + 1 : 0;
+
+                Cell[] neighbors =
+                {
+                    grid[jLeft,iTop],
+                    grid[j,iTop],
+                    grid[jRight,iTop],
+                    grid[jLeft,i],
+                    grid[jRight,i],
+                    grid[jLeft,iBottom],
+                    grid[j,iBottom],
+                    grid[jRight,iBottom]
+                };
+
+                grid[i, j].AddMultipleNeighbors(neighbors);
+            }
+        }
+    }
+
+    public void CellStatusUpdate(Cell cell)
+    {
+        switch (cell.NumOfAliveNeighbors)
+        { 
+            case var expression when (cell.NumOfAliveNeighbors < 2) || (cell.NumOfAliveNeighbors > 3): 
+                cell.IsAlive = false; 
+                break;
+            
+            case 3: 
+                cell.IsAlive = true; 
+                break;
+        }
+    }
+}
