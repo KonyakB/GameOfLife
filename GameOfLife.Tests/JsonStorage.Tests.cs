2f53fa49f4062486805676100e35d3b42216494b
phongsakon <pk@yeager.gmbh>
2024-03-17 09:08:57 +0100

    Refactor and simplify JsonStorage tests
    The JsonStorage unit tests have been revised and streamlined. The revision includes removal of a redundant stub class that was previously used for interfacing. The grid serialization and deserialization tests are now more focused and exhibit improved code clarity. Changes to the Grid and JsonStorage classes was necessary to expose certain properties for testing.

--- GameOfLife.Tests/JsonStorage.Tests.cs
+++ GameOfLife.Tests/JsonStorage.Tests.cs
@@ -1,204 +1,114 @@
-using Xunit;
 using GameOfLife.Components;
 using GameOfLife.Interfaces;
-using System.Collections.Generic;
 
 namespace GameOfLife.Tests
 {
     /// <summary>
-    /// Unit tests for the JsonStorage class.
+    /// Contains unit tests for the JsonStorage class.
     /// </summary>
     public class JsonStorageTests
     {
+        /// Represents an interface for saving and loading objects to/from JSON format.
+        /// /
+        private readonly IJsonStorage _jsonStorage;
+
         /// <summary>
-        /// Stub class implementing the <see cref="IJsonSerializer"/> interface.
+        /// Represents a class that provides methods for testing the JsonStorage class.
         /// </summary>
-        private class JsonSerializerStub : IJsonSerializer
+        public JsonStorageTests()
         {
-            /// <summary>
-            /// Represents a JSON serialized object.
-            /// </summary>
-            public string? SerializedObj { get; set; }
-
-            /// <summary>
-            /// Deserialize the specified JSON string into an object of type T.
-            /// </summary>
-            /// <typeparam name="T">The type of object to deserialize.</typeparam>
-            /// <param name="json">The JSON string to deserialize.</param>
-            /// <returns>An object of type T if deserialization is successful; otherwise, null.</returns>
-            public T Deserialize<T>(string? json)
-            {
-                return (T)(object)SerializedObj;
-            }
-
-            /// <summary>
-            /// Serializes an object to JSON format.
-            /// </summary>
-            /// <typeparam name="T">The type of the object to be serialized.</typeparam>
-            /// <param name="obj">The object to be serialized.</param>
-            /// <returns>A JSON string representing the serialized object.</returns>
-            public string? Serialize<T>(T obj)
-            {
-                bool[,] grid = obj as bool[,];
-                SerializedObj = SerializeGrid(grid);
-                return SerializedObj;
-            }
-
-            /// <summary>
-            /// Serializes a boolean grid into a JSON string.
-            /// </summary>
-            /// <param name="grid">The boolean grid to be serialized.</param>
-            /// <returns>The JSON string representing the serialized grid.</returns>
-            private string? SerializeGrid(bool[,] grid)
-            {
-                string? serialized = $"{{ \"rows\": {grid.GetLength(0)}, \"columns\": {grid.GetLength(1)}, \"grid\": [ ";
-                
-                for (int i = 0; i < grid.GetLength(0); i++)
-                {
-                    serialized += "[ ";
-                    
-                    for (int j = 0; j < grid.GetLength(1); j++)
-                    {
-                        serialized += $"{(grid[i, j] ? "true" : "false")}, ";
-                    }
-                    
-                    serialized = serialized.TrimEnd(',', ' ');
-                    serialized += "], ";
-                }
-                
-                serialized = serialized.TrimEnd(',', ' ');
-                serialized += "] }}";
-                
-                return serialized;
-            }
+            var fileStorage = new FileStorage();
+            var jsonSerializer = new JsonSerializerAdapter();
+            _jsonStorage = new JsonStorage(jsonSerializer, fileStorage);
         }
 
         /// <summary>
-        /// Stub implementation of the <see cref="IFileStorage"/> interface.
+        /// Saves the provided grid to a JSON file at the specified file path.
         /// </summary>
-        private class FileStorageStub : IFileStorage
-        {
-            /// <summary>
-            /// Represents a component that provides functionality to save and load objects to/from JSON format.
-            /// </summary>
-            public string? WrittenContent { get; private set; }
-
-            /// <summary>
-            /// Reads the content of a file at the given path.
-            /// </summary>
-            /// <param name="filePath">The path of the file to read.</param>
-            /// <returns>The content of the file.</returns>
-            public string? Read(string filePath)
-            {
-                return WrittenContent;
-            }
-
-            /// <summary>
-            /// Represents a component that provides functionality to save and load objects to/from JSON format.
-            /// </summary>
-            /// <typeparam name="T">The type of the object to save/load.</typeparam>
-            public void Write(string filePath, string? content)
-            {
-                WrittenContent = content;
-            }
-        }
-
-        /// <summary>
-        /// Saves an object to JSON format and writes it to a file using the specified file path.
-        /// </summary>
-        /// <typeparam name="T">The type of object to be serialized.</typeparam>
-        /// <param name="grid">The object to be serialized and saved.</param>
-        /// <param name="filePath">The path where the JSON file should be saved. If not specified, uses the default file path.</param>
-        /// <returns>A string representation of the serialized object.</returns>
-        public static IEnumerable<object[]> GridData()
-        {
-            yield return new object[]
-            {
-                "TestPath1",
-                new bool[,]
-                {
-                    { false, false, true, false, false },
-                    { false, false, false, true, false },
-                    { false, true, true, true, false },
-                    { false, false, false, false, false },
-                }
-            };
-            yield return new object[]
-            {
-                "TestPath2",
-                new bool[,]
-                {
-                    { true, true, false, true, false },
-                    { false, true, false, true, true },
-                    { true, false, true, false, false },
-                    { false, true, false, true, false },
-                }
-            };
-            yield return new object[]
-            {
-                "TestPath3",
-                new bool[,]
-                {
-                    { true, true, true, true, true },
-                    { true, true, true, true, true },
-                    { false, false, false, false, false },
-                    { false, false, false, false, false },
-                }
-            };
-        }
-
-        /// <summary>
-        /// Saves a grid object to a JSON file at the specified file path.
-        /// </summary>
-        /// <typeparam name="T">The type of the grid object.</typeparam>
-        /// <param name="grid">The grid object to be serialized to JSON.</param>
-        /// <param name="filePath">The path of the JSON file to save.</param>
-        /// <returns>The serialized JSON string.</returns>
+        /// <param name="grid">The grid to be saved.</param>
+        /// <param name="filePath">The file path where the JSON file should be saved.</param>
+        /// <returns>Returns the path of the saved JSON file.</returns>
         [Theory]
-        [MemberData(nameof(GridData))]
-        public void SaveToJson_MultipleGrids_ShouldSerializeGridAndWriteToFile(string filePath, bool[,] grid)
+        [InlineData(new bool[] { true, false, true }, 1)]
+        [InlineData(new bool[] { false, true, false, true, false }, 1)]
+        [InlineData(new bool[] { true, false, true, false, true, false, true }, 1)]
+        [InlineData(new bool[] { false, false, false, false, false, false, false }, 7)]
+        [InlineData(new bool[] { true, true, true, true, true, true, true }, 7)]
+        public void SaveToJsonTest(bool[] cellStates, int width)
         {
             // Arrange
-            var jsonSerializer = new JsonSerializerStub();
-            var fileStorage = new FileStorageStub();
-            var jsonStorage = new JsonStorage(jsonSerializer, fileStorage);
+            string _testJsonPath = Path.Combine(System.IO.Path.GetTempPath(), Path.GetRandomFileName());
+            int height = cellStates.Length / width;
+
+            bool[,] cellsOrig = new bool[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    cellsOrig[i, j] = cellStates[i * width + j];
+                }
+            }
+
+            Grid grid = new Grid(height, width, cellsOrig);
 
             // Act
-            jsonStorage.SaveToJson(grid, filePath);
 
-            // Assert
-            var expectedJson = jsonSerializer.Serialize(grid);
-            Assert.Equal(expectedJson, jsonSerializer.SerializedObj);
-            Assert.Equal(expectedJson, fileStorage.WrittenContent);
+            // Save grid to JSON
+            _jsonStorage.SaveToJson(grid, _testJsonPath);
+
+            // Assert if saved JSON is not empty
+            Assert.True(new FileInfo(_testJsonPath).Length > 0);
         }
 
-
         /// <summary>
-        /// Loads an object of type T from a JSON file.
+        /// Loads a grid from a JSON file.
         /// </summary>
-        /// <typeparam name="T">The type of object to load.</typeparam>
-        /// <param name="filePath">The path of the JSON file to load from.</param>
-        /// <returns>An object of type T if the loading is successful; otherwise, null.</returns>
-        [Fact]
-        public void LoadFromJson_ShouldReadFromFileAndDeserializeJson()
+        /// <param name="cellStates">The array of cell states.</param>
+        /// <param name="width">The width of the grid.</param>
+        [Theory]
+        [InlineData(new bool[] { false, false, false, false, false, false, false }, 7)]
+        [InlineData(new bool[] { true, true, true, true, true, true, true }, 7)]
+        public void LoadFromJsonTest(bool[] cellStates, int width)
         {
             // Arrange
-            var expectedGridString = @"{ ""rows"": 4, ""columns"": 5, ""grid"": [
-                                           [false, false, true, false, false],
-                                           [false, false, false, true, false],
-                                           [false, true, true, true, false],
-                                           [false, false, false, false, false]
-                                        ] }";
-            var jsonSerializer = new JsonSerializerStub() { SerializedObj = expectedGridString };
-            var fileStorage = new FileStorageStub();
-            fileStorage.Write("TestPath", expectedGridString);
-            var jsonStorage = new JsonStorage(jsonSerializer, fileStorage);
+            string _testJsonPath = Path.Combine(System.IO.Path.GetTempPath(), Path.GetRandomFileName());
+            int height = cellStates.Length / width;
+
+            bool[,] cellsOrig = new bool[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    cellsOrig[i, j] = cellStates[i * width + j];
+                }
+            }
+
+            Grid grid = new Grid(height, width, cellsOrig);
 
             // Act
-            var actualGridString = jsonStorage.LoadFromJson<string>("TestPath");
+
+            // Save grid to JSON
+            _jsonStorage.SaveToJson(grid, _testJsonPath);
+
+            // Load grid from JSON
+            Grid loadedGrid = _jsonStorage.LoadFromJson(_testJsonPath);
 
             // Assert
-            Assert.Equal(expectedGridString, actualGridString);
+
+            // Check dimensions of grids
+            Assert.Equal(grid.Rows, loadedGrid.Rows);
+            Assert.Equal(grid.Columns, loadedGrid.Columns);
+
+            // Check that grids are equal
+            for (int i = 0; i < grid.Rows; i++)
+            {
+                for (int j = 0; j < grid.Columns; j++)
+                {
+                    Assert.Equal(grid.DataGrid[i, j].IsAlive, loadedGrid.DataGrid[i, j].IsAlive);
+                }
+            }
         }
     }
 }
--- GameOfLife/src/Components/JsonStorage.cs
+++ GameOfLife/src/Components/JsonStorage.cs
@@ -2,14 +2,22 @@ using GameOfLife.Interfaces;
 
 namespace GameOfLife.Components;
 
-
+/// <summary>
+/// Represents a class that provides methods for saving and loading objects to/from JSON format.
+/// </summary>
 public class JsonStorage(IJsonSerializer jsonSerializer, IFileStorage fileStorage) : IJsonStorage
 
 {
-    
+    /// grid data.
     private const string FilePath = "grid.json";
 
 
+    /// <summary>
+    /// Saves a grid object to JSON format and writes it to a file.
+    /// </summary>
+    /// <param name="grid">The grid object to be saved.</param>
+    /// <param name="filePath">The file path to save the JSON data. If not specified, the default file path "grid.json" will be used.</param>
+    /// <returns>The JSON string representing the serialized grid object.</returns>
     public string? SaveToJson(Grid grid, string filePath = FilePath)
     {
         var flatGrid = FlattenGrid(grid.DataGrid);
@@ -30,6 +38,11 @@ public class JsonStorage(IJsonSerializer jsonSerializer, IFileStorage fileStorag
     }
 
 
+    /// <summary>
+    /// Loads a grid from a JSON file.
+    /// </summary>
+    /// <param name="filePath">The path of the JSON file to load from.</param>
+    /// <returns>The loaded grid if successful; otherwise, null.</returns>
     public Grid? LoadFromJson(string filePath)
     {
         var json = fileStorage.Read(filePath);
@@ -45,28 +58,40 @@ public class JsonStorage(IJsonSerializer jsonSerializer, IFileStorage fileStorag
         return new Grid(dataFromJson.Width, dataFromJson.Height, cells);
 
     }
-    
-    
+
+
+    /// <summary>
+    /// Flattens a 2D grid into a 1D array of cells.
+    /// </summary>
+    /// <param name="grid">The 2D grid to flatten.</param>
+    /// <returns>The flattened grid as a 1D array of cells.</returns>
     private Cell[] FlattenGrid(Cell[,]? grid)
     {
         if (grid == null)
             return Array.Empty<Cell>();
 
-        int width = grid.GetLength(0);
-        int height = grid.GetLength(1);
-        Cell[] flatGrid = new Cell[width * height];
+        int rows = grid.GetLength(0);
+        int columns = grid.GetLength(1);
+        Cell[] flatGrid = new Cell[rows * columns];
 
-        for (int i = 0; i < width; ++i)
+        for (int i = 0; i < rows; ++i)
         {
-            for (int j = 0; j < height; ++j)
+            for (int j = 0; j < columns; ++j)
             {
-                flatGrid[(i * width) + j] = grid[i, j];
+                flatGrid[(i * columns) + j] = grid[i, j]; // change here as well
             }
         }
 
         return flatGrid;
     }
 
+    /// <summary>
+    /// Unflattens a flat grid into a two-dimensional grid.
+    /// </summary>
+    /// <param name="flatGrid">The flat grid to unflatten.</param>
+    /// <param name="width">The width of the grid.</param>
+    /// <param name="height">The height of the grid.</param>
+    /// <returns>The unflattened two-dimensional grid.</returns>
     private bool[,] UnflattenGrid(List<Cell> flatGrid, int width, int height)
     {
         bool[,] grid = new bool[width, height];
